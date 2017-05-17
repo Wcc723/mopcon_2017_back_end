dd05b5806f2d5867a4c6566b2b0b90dc7e02ab2f
wcc723 <agito723@gmail.com>
2017-05-17 10:29:00 +0800

    修改成 ejs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var expressLayouts = require("express-ejs-layouts");
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -13,13 +14,14 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');  
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressLayouts);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
