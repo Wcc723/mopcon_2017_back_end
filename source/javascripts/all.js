0d5abf47ca7255016047d1a8e834c83813fb46d4
unaxie <una416@gmail.com>
2017-06-14 08:40:39 +0800

    update

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,12 @@
 $(function() {
-$('.navbar-toggle').on('click',function(){
-	$('.navbar-collapse').slideToggle();
-})
-
-
+	$('.navbar-toggle').on('click',function(){
+		$('.navbar-collapse').slideToggle();
+	});
+	positionMap();
 });
+
+function positionMap(){
+	var positionContentBoxHeight=$('.position_content_box').height();
+	$('.position_map').css('top',positionContentBoxHeight);
+	$('.position_content').css('height',positionContentBoxHeight+$('.position_map').height());
+}
